b09184be4888eccd2c323703fcff852819fd3fdc
Divya-Bodake <divyabodake25@gmail.com>
2022-11-19 17:08:58 +0530

    [Divya]Add:Adding Data in Reverse Order

--- LinkedListProblem/LinkedList.cs
+++ LinkedListProblem/LinkedList.cs
@@ -9,27 +9,43 @@ namespace LinkedListProblem
     public class LinkedList
     {
         public Node head;
-        internal void Add(int data)
+        //public void Add(int data)
+        //{
+        //    Node node = new Node(data);
+        //    if (this.head == null)
+        //    {
+        //        this.head = node;
+
+        //    }
+        //    else
+        //    {
+        //        Node temp = head;
+        //        while (temp.next != null)
+        //        {
+        //            temp = temp.next;
+        //        }
+        //        temp.next = node;
+        //    }
+        //    Console.WriteLine("{0} inserted into linked list", node.data);
+        //}
+
+        //UC2
+        public void AddReverseOrder(int data) 
         {
-            Node node = new Node(data);
+            Node newNode = new Node(data);
             if (this.head == null)
             {
-                this.head = node;
-
+                this.head = newNode;
             }
             else
             {
-                Node temp = head;
-                while (temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
             }
-            Console.WriteLine("{0} inserted into linked list", node.data);
         }
 
-        internal void Display()
+        public void Display()
         {
             Node temp = this.head;
             if (temp == null)
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -6,9 +6,14 @@
         {
             Console.WriteLine("Linked List Operation:");
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+
+            list.AddReverseOrder(70);
+            list.AddReverseOrder(30);
+            list.AddReverseOrder(56);
+
 
             list.Display();
         }
