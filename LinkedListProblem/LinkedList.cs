8e1f12b443f254d5cafb916685668d20e499c923
Divya-Bodake <divyabodake25@gmail.com>
2022-11-19 17:25:48 +0530

    [Divya]Add:Insert At Particular Position

--- LinkedListProblem/LinkedList.cs
+++ LinkedListProblem/LinkedList.cs
@@ -9,28 +9,28 @@ namespace LinkedListProblem
     public class LinkedList
     {
         public Node head;
-        //public void Add(int data)
-        //{
-        //    Node node = new Node(data);
-        //    if (this.head == null)
-        //    {
-        //        this.head = node;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
 
-        //    }
-        //    else
-        //    {
-        //        Node temp = head;
-        //        while (temp.next != null)
-        //        {
-        //            temp = temp.next;
-        //        }
-        //        temp.next = node;
-        //    }
-        //    Console.WriteLine("{0} inserted into linked list", node.data);
-        //}
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
 
         //UC2
-        public void AddReverseOrder(int data) 
+        public void AddReverseOrder(int data)
         {
             Node newNode = new Node(data);
             if (this.head == null)
@@ -45,6 +45,33 @@ namespace LinkedListProblem
             }
         }
 
+        //UC3
+        public Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
         public void Display()
         {
             Node temp = this.head;
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -6,16 +6,34 @@
         {
             Console.WriteLine("Linked List Operation:");
             LinkedList list = new LinkedList();
-            //list.Add(56);
-            //list.Add(30);
-            //list.Add(70);
 
-            list.AddReverseOrder(70);
-            list.AddReverseOrder(30);
-            list.AddReverseOrder(56);
+            Console.WriteLine("Select \n1.Create LinkedList \n 2.Add \n 3.Insert At Particular Position");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                  list.Add(56);
+                  list.Add(30);
+                  list.Add(70);
+                  list.Display();
+                  break;
+               case 2:
+                  list.AddReverseOrder(70);
+                  list.AddReverseOrder(30);
+                  list.AddReverseOrder(56);
+                  list.Display();
+                  break;
+               case 3:
+                    list.Add(56);
+                    list.Add(70);
+                    list.InsertAtParticularPosition(1, 30);
+                    list.Display();
+                    break;
 
-
-            list.Display();
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
         }
     }
 }
